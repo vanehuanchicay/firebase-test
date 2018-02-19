30db4ba81ba852bb288f3b36e396a3266b9d84f6
vanehuanchicay <vanesapardoh@gmail.com>
2018-02-19 17:51:51 -0300

    test II

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,25 +12,24 @@
   var provider = new firebase.auth.GoogleAuthProvider();
 
  function signIn(){
-   firebase.auth().signInWithPopup(provider).then(function(result) {
+   firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user.displayName);
-    $("p").text(user.displayName);
-
     // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 }
 
 
